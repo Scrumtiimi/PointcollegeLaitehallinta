f17190ccc5ec96b5eec5edee2e2b707425ba150f
Teemu Hyvärinen <teemu.hyvarinen@stud.pointcollege.fi>
2017-10-31 21:00:17 +0200

    rest api modifications

--- PointcollegeLaitehallinta/Controllers/RestController.cs
+++ PointcollegeLaitehallinta/Controllers/RestController.cs
@@ -1,4 +1,6 @@
-﻿using PointcollegeLaitehallinta.Models;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PointcollegeLaitehallinta.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,15 +12,12 @@ namespace PointcollegeLaitehallinta.RestControllers
 {
     [RoutePrefix("api/laitteet")]
     public class RestController : ApiController {
-
-        
-
         
         [Route("")]
         [HttpGet]
         public IEnumerable<Laitteet> GetLaitteet() {
 
-            LaitehallintaEntities ent = new LaitehallintaEntities();
+            LaitehallintaEntities2 ent = new LaitehallintaEntities2();
 
             List<Laitteet> laitteet = new List<Laitteet>();
             laitteet = ent.Laitteets.ToList();
@@ -30,7 +29,7 @@ namespace PointcollegeLaitehallinta.RestControllers
         [HttpGet]
         public Laitteet GetLaitteet(int id) {
 
-            LaitehallintaEntities ent = new LaitehallintaEntities();
+            LaitehallintaEntities2 ent = new LaitehallintaEntities2();
 
             var laite = (from l in ent.Laitteets
                          where l.Laitetyypit.Laitetyyppi == id
@@ -40,27 +39,80 @@ namespace PointcollegeLaitehallinta.RestControllers
 
         [Route("")]
         [HttpPost]
-        public void AddLaite([FromBody] Laitteet laitteet) {
+        public void AddLaite([FromBody] Laitteet laite) {
 
-            LaitehallintaEntities ent = new LaitehallintaEntities();
+            LaitehallintaEntities2 ent = new LaitehallintaEntities2();
+
+            try {
+                var getLaite = ent.Laitteets.Count(l => l.Laitetyypit.Laitetyyppi == laite.Laitetyypit.Laitetyyppi);
+                //check if posted laite object doesn't contain laitetyyppi then it will create new one 
+                if(getLaite < 1) {
+                    Guid LaiteUIDNew = Guid.NewGuid();
+                    Guid LaiteTyyppiUIDNew = Guid.NewGuid();
+                    Guid LaiteVarastopaikkaGUID = Guid.NewGuid();
+
+                    //create new instance of laitteet and laitetyypit assing values from json object
+                    Laitteet newLaite = new Laitteet();
+                    newLaite.Laitenimi = laite.Laitenimi;
+                    newLaite.Laitetyyppi_uid = LaiteTyyppiUIDNew;
+                    newLaite.Laite_uid = LaiteUIDNew;
+                    newLaite.Sarjanumero = laite.Sarjanumero;
+                    newLaite.Merkki = laite.Merkki;
+                    newLaite.Malli = laite.Malli;
+                    newLaite.Hankitapaiva = laite.Hankitapaiva;
+                    newLaite.Muisti = laite.Muisti;
+                    newLaite.Kovalevynkoko = laite.Kovalevynkoko;
+                    newLaite.Varastopaikka_uid = LaiteVarastopaikkaGUID;
+                    newLaite.Lisatietoja = laite.Lisatietoja;
+                    newLaite.Varastopaikat = laite.Varastopaikat;
+
+                    Laitetyypit newLaiteTyyppi = new Laitetyypit();
+                    newLaiteTyyppi.Laitetyyppi_uid = LaiteTyyppiUIDNew;
+                    newLaiteTyyppi.Laitetyyppi = laite.Laitetyypit.Laitetyyppi;
+                    newLaiteTyyppi.Laitetyyppinimi = laite.Laitetyypit.Laitetyyppinimi;
+                    newLaiteTyyppi.Lisatietoja = laite.Lisatietoja;
+
+                    //only varastopaikka_uid at the moment as otherwise there is constraint error on database
+                    //most likely this object will consist more data
+                    Varastopaikat newVarastoPaikka = new Varastopaikat();
+                    newVarastoPaikka.Varastopaikka_uid = LaiteVarastopaikkaGUID;
 
 
-            //toimii!!!
-            //muista lisätä samanlainen objecti myös varastopaikoista
-            ent.Laitteets.Add(new Laitteet {
-                Laitetyypit = laitteet.Laitetyypit,
-                Lisatietoja = laitteet.Lisatietoja,
-                Tapahtumalokits = laitteet.Tapahtumalokits,
-                Laitenimi = laitteet.Laitenimi,
-                Sarjanumero = laitteet.Sarjanumero,
-                Merkki = laitteet.Merkki,
-                Hankitapaiva = laitteet.Hankitapaiva,
-                Muisti = laitteet.Muisti,
-                Kovalevynkoko = laitteet.Kovalevynkoko
-            });
+
+                    ent.Laitteets.Add(newLaite);
+                    ent.Laitetyypits.Add(newLaiteTyyppi);
+                    ent.Varastopaikats.Add(newVarastoPaikka);
+
+                    ent.SaveChanges();
+
+                }
+           
+            } catch (Exception e) {
+                Console.WriteLine("Tietokannasta löytyy jo kyseinen tieto", e);
+            }
+
+           
+            
+        }
+
+        [Route("muutatiedot")]
+        [HttpPost]
+        public void ModifyLaite([FromBody] Laitteet laite) {
+            LaitehallintaEntities2 ent = new LaitehallintaEntities2();
+
+            Laitteet updateLaite = ent.Laitteets.Single(l => l.Laitetyypit.Laitetyyppi == laite.Laitetyypit.Laitetyyppi);
+
+            if(updateLaite != null) {
+                updateLaite.Laitenimi = laite.Laitenimi;
+                updateLaite.Kovalevynkoko = laite.Kovalevynkoko;
+                updateLaite.Lisatietoja = laite.Lisatietoja;
+                updateLaite.Malli = laite.Malli;
+                updateLaite.Muisti = laite.Muisti;
+
+            }
 
             ent.SaveChanges();
-            
+            ent.Dispose();
         }
     }
 }
